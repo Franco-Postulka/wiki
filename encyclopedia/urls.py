c736e6083b6ac1b042869d8200d080e6cd16dd4e
Franco-Postulka <francopostulka@hotmail.com>
2024-01-08 16:10:32 -0300

    Best way found at the moment for de serch bar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path('wiki/<str:entry>', views.search, name='search')
+    path('wiki/<str:entry>', views.search, name='search'),
+    path('search_bar',views.search_bar, name='search_bar')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 import markdown2
 from . import util
 from django import forms
@@ -22,8 +24,61 @@ def search(request, entry):
             "entry_title": entry
         })
 
-class NewTaskForm(forms.Form):
-    task = forms.CharField(label='New Task')
+def compare_str(str1,str2):
+    """_summary: compare 2 strings and returns the number of coincidences
+    Args:
+        str1 (str)
+        str2 (str)
+    Returns:
+        int: numbrer of coincidences
+    """
+    
+    import re
+    contador = 0
+    if len(str1)<= len(str2):
+        for i in range(len(str1)):
+            coincidences = re.search(str1[i].lower(),str2[i].lower())
+            if coincidences != None:
+                contador += 1
+    else:
+        for i in range(len(str2)):
+            coincidences = re.search(str1[i].lower(),str2[i].lower())
+            if coincidences != None:
+                contador += 1
+    return contador
 
+def find_coincidences(list,string):
+    """_summary: receives a list and a string, returns a list with elements of the list it received
+    that matched the string
+    Args:
+        list (list):
+        string (str)
+    Returns:
+        list: the elements that matched the string
+    """
+    new_list = []
+    for element in list:
+        coincidences = compare_str(string,element)
+        if coincidences >= 1:
+            new_list.append(element)
+    return new_list
 
-
+def search_bar(request):
+    if request.method == "POST":
+        search = request.POST['q']
+        entry = util.get_entry(search)
+        if entry != None:
+            html_content = markdown2.markdown(entry)
+            return render(request, "encyclopedia/entry.html", {
+                "entry_content": html_content,
+                "entry_title": search
+            })
+        else:
+            entries = util.list_entries()
+            coincidences = find_coincidences(entries,search)
+            if len(coincidences) > 0:
+                return render(request, "encyclopedia/index.html", {
+                "entries": coincidences
+                })
+            else:
+                return render(request,  "encyclopedia/notfound.html")
